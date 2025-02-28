fa2e3d0ce18cd2fe20e2bc7205b1120acaa5e06b
Kapil Mathur <kapil.mathur1987@gmail.com>
2025-02-28 17:25:10 +0530

    ML.py - A simple script implementing normal and gradient descent
    Just a toy problem. Generate some data using numpy, implement
    normal equation and gradient descent also in numpy

---
+++ ML/ML.py
@@ -0,0 +1,76 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def generate_data():
+  X = 2*np.random.rand(100,1)
+  y = 4 + 3*X + np.random.randn(100,1)
+  return X,y
+  
+
+def normal_equation():
+  X,y = generate_data()
+  X_b = np.c_[np.ones((100,1)), X]
+  theta_best = np.linalg.inv(X_b.T.dot(X_b)).dot(X_b.T).dot(y)
+  X_new = np.array([[0], [2]])
+  X_new_b = np.c_[np.ones((2,1)), X_new]
+  y_predict = X_new_b.dot(theta_best)
+
+  plt.plot(X_new, y_predict, "r-")
+  plt.plot(X,y,"b.")
+  plt.show()
+
+def gradient_descent():
+  X,y = generate_data()
+  X_b = np.c_[np.ones((100,1)), X]
+  learning_rate = 0.1
+  iterations = 1000
+  theta = np.random.randn(2,1)
+  m = 100
+
+  for iteration in range(iterations):
+    gradiants = (2/m)*X_b.T.dot(X_b.dot(theta) - y)
+    theta = theta - (learning_rate*gradiants)
+
+  X_new = np.array([[0], [2]])
+  X_b_new = np.c_[np.ones((2,1)), X_new]
+  y_predict = X_b_new.dot(theta)
+
+  plt.plot(X_new, y_predict, "r-")
+  plt.plot(X,y, "b.")
+  plt.show()
+
+def stochastic_gradient_descent():
+  X,y = generate_data()
+  X_b = np.c_[np.ones((100,1)), X]
+  n_epochs = 50
+  theta = np.random.randn(2,1)
+  m = 100
+  t0,t1 = 5,50
+
+  for epoch in range(n_epochs):
+    for i in range(m):
+      random_index = np.random.randint(m)
+      xi = X_b[random_index:random_index+1]
+      yi = y[random_index:random_index+1]
+
+      gradiant = 2*xi.T.dot(xi.dot(theta) - yi)
+      learning_schedule = t0/((epoch*m + i) + t1)
+      theta = theta - (learning_schedule*gradiant)
+
+  X_new = np.array([[0], [2]])
+  X_b_new = np.c_[np.ones((2,1)), X_new]
+  y_predict = X_b_new.dot(theta)
+
+  plt.plot(X_new, y_predict, "r-")
+  plt.plot(X,y, "b.")
+  plt.show()
+
+
+
+  
+
+if __name__ == "__main__":
+ # normal_equation()
+  stochastic_gradient_descent()
+
+
